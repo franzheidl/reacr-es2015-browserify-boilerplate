bfa8861477e2ad0a4e3d762cff9555f81601f039
Franz Heidl <franz@franzheidl.de>
2016-09-14 08:42:18 +0200

    Initial commit with code

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestComponent = require('./TestComponent.jsx');
+
+
+ReactDOM.render(<TestComponent/>, document.getElementById('app'));
